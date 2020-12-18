92e18cfb3aaf2e2284dec469b051ecf78b5e22f2
tnahuelpan <73675592+tnahuelpan@users.noreply.github.com>
2020-12-17 22:01:26 -0300

    Añade STAGE_NAME

--- gradle.groovy
+++ gradle.groovy
@@ -1,9 +1,11 @@
 def call(){
   
 	stage('Build & test') {
+		STAGE_NAME = 'Build & test'
 		sh "gradle clean build"
 	}
 	stage('Sonar') {
+		STAGE_NAME = 'sonar'
 		//Generar instancia de tipo tool del scanner
 		//Va el nombre de la instancia en Jenkins>Global tool config.
 		def scannerHome = tool 'sonar-scanner';
@@ -13,13 +15,16 @@ def call(){
 		}
 	}
 	stage('Run') {
+		STAGE_NAME = 'Run'
 		sh "nohup gradle bootRun &"
 		sleep 30
 	}
 	stage('Test') {
+		STAGE_NAME = 'Test'
 		sh "curl -XGET 'localhost:8082/rest/mscovid/test?msg=testing'"
 	}
 	stage('Nexus') {
+		STAGE_NAME = 'Nexus'
 		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'maven-releases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'D:\\Estudios\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.3']]]
 	}
 }
